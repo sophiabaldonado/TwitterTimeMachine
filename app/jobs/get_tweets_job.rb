582982cad1548bea020b482b305d53ada832e8f4
sophia <sophiabaldonado@gmail.com>
2016-06-05 15:16:02 -0700

    Create a job that retrieved tweets and stores in db;

--- app/jobs/get_tweets_job.rb
+++ app/jobs/get_tweets_job.rb
@@ -1,7 +1,20 @@
 class GetTweetsJob < ActiveJob::Base
   queue_as :default
 
-  def get_tweets
-    # Do something later
+  OFFSET = 3600
+
+  def self.get_tweets
+    tweets = TwitterClient.search("a", result_type: "recent", count: 5).to_a
+    tweets.each do |tweet|
+      t = TimeTweet.new
+      t.text = tweet.text
+      t.user = tweet.user
+      t.popularity = tweet.retweet_count + tweet.favorite_count
+      t.tweeted_at = tweet.created_at
+      t.timezone = tweet.user.utc_offset / OFFSET
+      t.coordinates = tweet.geo.coordinates
+
+      t.save
+    end
   end
 end

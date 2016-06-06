46455219235213d27780ad2219922666b2e1c0c4
sophia <sophiabaldonado@gmail.com>
2016-06-06 07:54:39 -0700

    Calculate correct timezone and other info based on their location;

--- app/controllers/time_tweets_controller.rb
+++ app/controllers/time_tweets_controller.rb
@@ -1,22 +1,32 @@
 class TimeTweetsController < ApplicationController
-
-
   def index
     @time_tweet = TimeTweet.new(nil)
     # @place_tweet = TimeTweet.find_place
-    @src = "https://www.google.com/maps/embed/v1/search?key=#{ENV["GOOGLE_SERVER_KEY"]}&q=record+stores+in+Seattle"
+    # @src = "https://www.google.com/maps/embed/v1/search?key=#{ENV["GOOGLE_SERVER_KEY"]}&q=record+stores+in+Seattle"
   end
 
   def create
-    @popular_tweet = TimeTweet.popular_tweet(params["timewarp"])
-    @embeded_tweet = TwitterClient.oembed(@popular_tweet.twitter_id)
-    @most_recent_tweet = TimeTweet.most_recent_tweet(params["timewarp"])
-    @random_tweet = TimeTweet.random_tweet(params["timewarp"])
+    timewarp = params["timewarp"]
+    zone = params["timezone"]
+
+    popular_tweet = TimeTweet.popular_tweet(timewarp, zone)
+    most_recent_tweet = TimeTweet.most_recent_tweet(timewarp, zone)
+    random_tweet = TimeTweet.random_tweet(timewarp, zone)
+
+    @display_time = TimeTweet.time(timewarp, zone, popular_tweet.tweeted_at)
+    @sunrise_time = TimeTweet.time(timewarp, zone, popular_tweet.tweeted_at)
+    @sunset_time = TimeTweet.time(timewarp, zone, popular_tweet.tweeted_at)
+    @adjusted_timezone = timewarp.to_i + zone.to_i
+
+    @embeded_popular_tweet = TwitterClient.oembed(popular_tweet.twitter_id)
+    @embeded_most_recent_tweet = TwitterClient.oembed(most_recent_tweet.twitter_id)
+    @embeded_random_tweet = TwitterClient.oembed(random_tweet.twitter_id)
+
+    params["timewarp"].to_i < 0 ? @selected_time = "Past" : @selected_time = "Future"
     render :show
   end
 
   def show
-
   end
 
 end
--- app/models/time_tweet.rb
+++ app/models/time_tweet.rb
@@ -1,9 +1,11 @@
+require 'timezone'
+
 class TimeTweet < ActiveRecord::Base
 
-  def self.find_tweets(timewarp)
-    timewarp = timewarp.to_i
+  def self.find_tweets(timewarp, starting_timezone)
+    offset = timewarp.to_i + starting_timezone.to_i
 
-    zoned_tweets = self.zoned_tweets(timewarp)
+    zoned_tweets = self.zoned_tweets(offset)
 
     minutes_passed = 15
     current_tweets = self.recent_tweets(zoned_tweets, minutes_passed)
@@ -30,8 +32,8 @@ class TimeTweet < ActiveRecord::Base
     zoned_tweets(timewarp, (timezone_offset + 1), tweets)
   end
 
-  def self.popular_tweet(timewarp)
-    current_tweets = self.find_tweets(timewarp)
+  def self.popular_tweet(timewarp, starting_timezone)
+    current_tweets = self.find_tweets(timewarp, starting_timezone)
 
     if current_tweets.empty?
       tweet = ["Sorry No Tweets"]
@@ -42,8 +44,8 @@ class TimeTweet < ActiveRecord::Base
     return tweet
   end
 
-  def self.most_recent_tweet(timewarp)
-    current_tweets = self.find_tweets(timewarp)
+  def self.most_recent_tweet(timewarp, starting_timezone)
+    current_tweets = self.find_tweets(timewarp, starting_timezone)
 
     if current_tweets.empty?
       tweet = ["Sorry No Tweets"]
@@ -54,8 +56,8 @@ class TimeTweet < ActiveRecord::Base
     return tweet
   end
 
-  def self.random_tweet(timewarp)
-    current_tweets = self.find_tweets(timewarp)
+  def self.random_tweet(timewarp, starting_timezone)
+    current_tweets = self.find_tweets(timewarp, starting_timezone)
 
     if current_tweets.empty?
       tweet = ["Sorry No Tweets"]
@@ -65,4 +67,45 @@ class TimeTweet < ActiveRecord::Base
 
     return tweet
   end
+
+  def self.time(timewarp, starting_timezone, tweet_time)
+    timezone_offset = timewarp.to_i + starting_timezone.to_i
+
+    coords = self.zone_coords(timezone_offset)
+    timezone = Timezone.lookup(coords[0], coords[1])
+    timezone.utc_to_local(tweet_time)
+  end
+
+  def self.zone_coords(offset)
+    # prolly find an api that does this -_-;
+    zone_coords = {
+      "12"  => [-17.734978, 168.322821],
+      "11"  => [-33.916990, 151.165184],
+      "10"  => [35.835973, 139.148079],
+      "9"   => [14.507375, 121.018727],
+      "8"   => [13.751229, 100.727425],
+      "7"   => [50.999333, 71.787514],
+      "6"   => [27.709918, 68.175791],
+      "5"   => [19.355895, 56.701688],
+      "4"   => [-21.799596, 46.287599],
+      "3"   => [39.886387, 33.028011],
+      "2"   => [9.056341, 7.412197],
+      "1"   => [51.5287352,-0.3817818],
+      "0"   => [14.948879, -23.517330],
+      "-1"  => [-54.159730, -36.712747],
+      "-2"  => [61.157936, -45.427781],
+      "-3"  => [-16.494469, -68.033072],
+      "-4"  => [40.745270, -73.977763],
+      "-5"  => [19.421347, -99.129364],
+      "-6"  => [62.447821, -114.379475],
+      "-7"  => [47.617755, -122.332083],
+      "-8"  => [61.200744, -149.721082],
+      "-9"  => [-17.550479, -149.482197],
+      "-10" => [-19.131769, -169.884360],
+      "-11" => [0.811671, -176.618611],
+      "-12" => [-36.865823, 174.789578]
+    }
+    zone_coords[offset.to_s]
+  end
+
 end
